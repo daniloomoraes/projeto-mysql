1be4465d0fa3e9b1d426216d7cdde955ba4e6673
Danilo Oliveira <daniloomoraes@gmail.com>
2023-07-14 18:49:38 -0300

    feat: add map itens result

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 function App() {
 
-  axios.get('http://localhost:3001/api/teste').then(resp => {
-    console.log(resp);
-  });
-  
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/teste')
+      .then(resp => {
+        setData(resp.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
-      teste
+      {data.map(item => (
+        <div key={item.id}>{item.nome}</div>
+      ))}
     </div>
   );
 }
